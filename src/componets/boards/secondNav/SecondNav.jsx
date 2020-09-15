cc8459f001cd70585664c1e62837df165c96b090
Samuel Romero Arbelo <oiranca@gmail.com>
2020-09-15 16:16:22 +0100

    New Modal Components

--- src/componets/boards/secondNav/SecondNav.jsx
+++ src/componets/boards/secondNav/SecondNav.jsx
@@ -1,83 +1,115 @@
-import React from "react";
+import React, {useState} from "react";
 import './SecondNav.scss'
 import {Button, Form, FormControl, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCalendarAlt, faEye, faConciergeBell, faEllipsisH, faStar} from '@fortawesome/free-solid-svg-icons'
+import ModalBoardsType from "../../modals/ModalBoardType";
+import ModalBoardsVisibility from "../../modals/ModalBoardVisibility";
 
 const SecondNav = (props) => {
+
+
+    const [show, setShow] = useState(false);
+
+
+    const showModal = () => {
+        if (show === false) {
+            setShow(true);
+
+        } else {
+            setShow(false);
+
+        }
+
+       setShowBis(false);
+    };
+
+
+
+    const [showBis, setShowBis] = useState(false);
+
+
+    const showModalBis = () => {
+        if (showBis === false) {
+            setShowBis(true);
+
+        } else {
+            setShowBis(false);
+
+        }
+
+     setShow(false);
+    };
+
     return (
+        <React.Fragment>
+            <Navbar className="secondNav" expand="lg">
+                <Form inline>
+                    <FormControl type="text" placeholder="Here board name" className="mr-sm-2"/>
+                </Form>
 
-        <Navbar className="secondNav" expand="lg">
-            <Form inline>
-                <FormControl type="text" placeholder="Here board name" className="mr-sm-2"/>
-            </Form>
-            <Navbar.Toggle/>
-            <Navbar.Collapse className="justify-content-end">
-                <Nav className="mr-auto">
-                    <div id="starNav">
-                        <FontAwesomeIcon icon={faStar}/>
-                    </div>
-
-                    <div className="buttonType">
-                        <NavDropdown title="Board Type" id="dropType">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider/>
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                    </div>
-
-
-                    <div className="buttonBigWidth">
-                        <FontAwesomeIcon className="iconsFontAwesome" icon={faEye}/>
-                        <NavDropdown title="Visibility Status" id="dropVisibility">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider/>
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                    </div>
-
-
-                </Nav>
-                <Nav className="navRight">
-                    <Nav.Link href="#link">
-                        <div className="buttonMediumWidth">
-                            <FontAwesomeIcon className="iconsFontAwesome" icon={faCalendarAlt}/>
-                            <span>Calendar</span>
+                    <Nav className="mr-auto navLeft">
+                        <div id="starNav">
+                            <FontAwesomeIcon icon={faStar}/>
                         </div>
-                    </Nav.Link>
-                    <div className="buttonMediumWidth">
-                        <FontAwesomeIcon className="iconsFontAwesome" icon={faConciergeBell}/>
-                        <NavDropdown title="Butler" id="dropButler">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider/>
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                    </div>
+                        <Nav.Link onClick={showModal}>
 
+                            <div className="buttonBigWidth">
 
-                    <div className="buttonBigWidth">
-                        <FontAwesomeIcon className="iconsFontAwesome" icon={faEllipsisH}/>
-                        <NavDropdown title="Mostrar Menú" id="dropVisibility">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider/>
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
+                                <span>  Board Type</span>
 
-                    </div>
+                            </div>
+                        </Nav.Link>
+                        <Nav.Link onClick={showModalBis}>
+                            <div className="buttonBigWidth">
+                                <FontAwesomeIcon className="iconsFontAwesome" icon={faEye}/>
 
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+                                <span>Visibility Status</span>
 
+                            </div>
+                        </Nav.Link>
 
+                    </Nav>
+                    <Nav className="navRight">
+                        <Nav.Link href="#link">
+                            <div className="buttonMediumWidth">
+                                <FontAwesomeIcon className="iconsFontAwesome" icon={faCalendarAlt}/>
+                                <span>Calendar</span>
+                            </div>
+                        </Nav.Link>
+                        <Nav.Link href="#link">
+                            <div className="buttonMediumWidth">
+                                <FontAwesomeIcon className="iconsFontAwesome" icon={faConciergeBell}/>
+                                <span>Butler</span>
+                            </div>
+                        </Nav.Link>
+
+                        <div className="buttonBigWidth">
+                            <FontAwesomeIcon className="iconsFontAwesome" icon={faEllipsisH}/>
+                            <NavDropdown title="Mostrar Menú" id="dropsNavs">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider/>
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+
+                        </div>
+
+                    </Nav>
+
+            </Navbar>
+            <div className="containerModals">
+                <div className="modalType">
+
+                    <ModalBoardsType value={show}/>
+                </div>
+                <div className="modalVisibility">
+
+                    <ModalBoardsVisibility show={showBis}/>
+                </div>
+            </div>
+        </React.Fragment>
     );
 }
 
